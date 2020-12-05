4e75fb2e8fe55559286b541925ffc57d6d0c80d9
Charpandur <charlottepandur@outlook.com>
2020-12-06 00:45:56 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// Answer question 1
+// FirstVar not initialized with a value.
+
+var firstVar;
+
+// Answer question 2
+// Declared a variable called name and assigned it my first name.
+var name = "charlotte";
+console.log(name);
+
+// Answer question 3
+// Declared and initialized a variable with a number value.
+var age = 28;
+console.log(age);
+
+// Answer question 4
+// Created a variable called division and initialized it with a value of 20 divided by 5.
+var division = 20 / 5;
+
+// Answer question 5
+// Check the typeof the value "frog".
+typeof "frog";
+
+// Answer question 6
+// Declared and initialized a variable called orderHasShipped with a boolean value.
+var orderHasShipped = true;
+
+// Answer question 7
+// Created an if statement that checks if orderHasShipped is true. If it is true,
+// Console log the string value "true". If not, console log the string value "false".
+if (orderHasShipped === true) {
+  console.log("Order Has been shipped!");
+} else {
+  console.log("fail");
+}
+
+// Answer question 8
+// Created a for loop that counts from 0 to 9.
+// Console log the value of the counter variable inside the loop.
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
